d629fb24c87b4db4f0962c815de4ae6bea7ae176
Jason Terhorst <jason.terhorst@precisionplanting.com>
2015-08-20 13:26:23 -0500

    Added step counting

--- FitWatch WatchKit Extension/InterfaceController.m
+++ FitWatch WatchKit Extension/InterfaceController.m
@@ -7,10 +7,11 @@
 //
 
 #import "InterfaceController.h"
-
+@import CoreMotion;
 
 @interface InterfaceController()
-
+@property (nonatomic, strong) CMPedometer * pedometer;
+@property (nonatomic, assign) NSInteger stepCount;
 @end
 
 
@@ -25,11 +26,31 @@
 - (void)willActivate {
     // This method is called when watch view controller is about to be visible to user
     [super willActivate];
+    
+    if ([CMPedometer isStepCountingAvailable]) {
+        NSLog(@"step counter ready!");
+        _pedometer = [[CMPedometer alloc] init];
+        NSDate * startOfToday = [[NSCalendar currentCalendar] startOfDayForDate:[NSDate date]];
+        [_pedometer startPedometerUpdatesFromDate:startOfToday withHandler:^(CMPedometerData * data, NSError * error) {
+            _stepCount = _stepCount + [[data numberOfSteps] integerValue];
+            NSNumberFormatter * outputFormatter = [[NSNumberFormatter alloc] init];
+            outputFormatter.usesGroupingSeparator = YES;
+            outputFormatter.groupingSeparator = [[NSLocale currentLocale] objectForKey:NSLocaleGroupingSeparator];
+            outputFormatter.groupingSize = 3;
+            outputFormatter.zeroSymbol = @"--";
+            _stepOutputLabel.text = [outputFormatter stringFromNumber:@(_stepCount)];
+        }];
+    } else {
+        NSLog(@"can't get step counts");
+        _stepOutputLabel.text = @"--";
+    }
 }
 
 - (void)didDeactivate {
     // This method is called when watch view controller is no longer visible
     [super didDeactivate];
+    [_pedometer stopPedometerUpdates];
+    _pedometer = nil;
 }
 
 @end
--- FitWatch/ViewController.m
+++ FitWatch/ViewController.m
@@ -7,21 +7,47 @@
 //
 
 #import "ViewController.h"
+@import CoreMotion;
 
 @interface ViewController ()
-
+@property (nonatomic, strong) CMPedometer * pedometer;
+@property (nonatomic, assign) NSInteger stepCount;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    if ([CMPedometer isStepCountingAvailable]) {
+        NSLog(@"step counter ready!");
+        _pedometer = [[CMPedometer alloc] init];
+        NSDate * startOfToday = [[NSCalendar currentCalendar] startOfDayForDate:[NSDate date]];
+        [_pedometer startPedometerUpdatesFromDate:startOfToday withHandler:^(CMPedometerData * data, NSError * error) {
+            _stepCount = _stepCount + [[data numberOfSteps] integerValue];
+            NSNumberFormatter * outputFormatter = [[NSNumberFormatter alloc] init];
+            outputFormatter.usesGroupingSeparator = YES;
+            outputFormatter.groupingSeparator = [[NSLocale currentLocale] objectForKey:NSLocaleGroupingSeparator];
+            outputFormatter.groupingSize = 3;
+            outputFormatter.zeroSymbol = @"--";
+            _stepOutputLabel.text = [outputFormatter stringFromNumber:@(_stepCount)];
+        }];
+    } else {
+        NSLog(@"can't get step counts");
+        _stepOutputLabel.text = @"--";
+    }
+}
+
+- (void)dealloc
+{
+    [_pedometer stopPedometerUpdates];
+    _pedometer = nil;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 @end
